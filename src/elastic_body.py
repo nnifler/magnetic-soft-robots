7354e39198b25cbd3895db8f875d1d71150ae62e
Finn <108729205+LordFrostwolf@users.noreply.github.com>
2024-11-21 13:52:32 +0100

    init

---
+++ src/elastic_body.py
@@ -0,0 +1,45 @@
+import os
+
+def createElasticObject(root, name, poissonRatio, youngModulus, magneticForce, magneticDir, showForce):
+    cwd = os.getcwd()
+    ## Add Object
+    elastic_obj = root.addChild('object')
+    elastic_obj.addObject('EulerImplicitSolver', name="cg_odesolver", rayleighStiffness=0.1, rayleighMass=0.1)
+    elastic_obj.addObject('SparseLDLSolver', name="linear_solver", template="CompressedRowSparseMatrixMat3x3d")
+    elastic_obj.addObject('MeshGmshLoader', name="meshLoader", filename=f"{cwd}/meshes/{name}.msh")
+    elastic_obj.addObject('TetrahedronSetTopologyContainer', name="topo", src="@meshLoader")
+    elastic_obj.addObject('MechanicalObject', name="dofs", src="@meshLoader")
+    elastic_obj.addObject('TetrahedronSetGeometryAlgorithms', template="Vec3d", name="GeomAlgo")
+    elastic_obj.addObject('DiagonalMass', name="Mass", massDensity=1.0)
+    elastic_obj.addObject('TetrahedralCorotationalFEMForceField', template="Vec3d", name="FEM", method="large", poissonRatio=poissonRatio, youngModulus=youngModulus, computeGlobalMatrix=False)
+
+    ## Add Constraints
+    elastic_obj.addObject('FixedConstraint', name="FixedConstraint", indices="0 1 2 3")
+    elastic_obj.addObject('LinearSolverConstraintCorrection')
+
+    ## Add Magnetic Field
+    fx = magneticForce * magneticDir[0]
+    fy = magneticForce * magneticDir[1]
+    fz = magneticForce * magneticDir[2]
+    elastic_obj.addObject('ConstantForceField', name="mag_force", forces=f"{fx} {fy} {fz}", showArrowSize="0.1" if showForce else "0")
+
+    ## Add Surface
+    surf = elastic_obj.addChild('ExtractSurface')
+    surf.addObject('TriangleSetTopologyContainer', name="Container", position="@../topo.position")
+    surf.addObject('TriangleSetTopologyModifier', name="Modifier")
+    surf.addObject('Tetra2TriangleTopologicalMapping', name="SurfaceExtractMapping", input="@../topo", output="@Container")
+
+    ## Add collision
+    collision = surf.addChild('Surf')
+    collision.addObject('TriangleSetTopologyContainer', name="Container", src="@../Container")
+    collision.addObject('MechanicalObject', name="surfaceDOFs")
+    collision.addObject('PointCollisionModel', name="CollisionModel")
+    collision.addObject('IdentityMapping', name="CollisionMapping", input="@../../dofs", output="@surfaceDOFs")
+
+    ## Add visuals
+    visu = elastic_obj.addChild("VisualModel")
+    visu.loader = visu.addObject('MeshOBJLoader', name="loader", filename=f"{cwd}/meshes/{name}.obj")
+    visu.addObject('OglModel', name="model", src="@loader", scale3d=[1]*3, color=[1., 1., 1.], updateNormals=False)
+    visu.addObject('BarycentricMapping')
+
+    return elastic_obj
---
+++ src/sofa_instantiator.py
@@ -0,0 +1,82 @@
+from elastic_body import createElasticObject
+# Required import for python
+import Sofa
+
+
+# Choose Parameters
+USE_GUI = True
+USE_GRAVITY = True
+NAME = "beam"
+POISSON_RATIO = 0.45
+YOUNG_MODULUS = 10000
+MAGNETIC_FORCE = 10
+MAGNETIC_DIR = (0,-1,0)
+SHOW_FORCE = False
+
+
+def main():
+    import SofaRuntime
+    import Sofa.Gui
+
+    root = Sofa.Core.Node("root")
+    createScene(root)
+    Sofa.Simulation.init(root)
+
+    if not USE_GUI:
+        for iteration in range(10):
+            Sofa.Simulation.animate(root, root.dt.value)
+    else:
+        Sofa.Gui.GUIManager.Init("myscene", "qglviewer")
+        Sofa.Gui.GUIManager.createGUI(root, __file__)
+        Sofa.Gui.GUIManager.SetDimension(1080, 1080)
+        Sofa.Gui.GUIManager.MainLoop(root)
+        Sofa.Gui.GUIManager.closeGUI()
+
+
+def createScene(root):
+    root.gravity=[0, -9.81 if USE_GRAVITY else 0, 0]
+    root.dt=0.005
+
+    root.addObject("RequiredPlugin", pluginName=[
+    'Sofa.Component.Collision.Detection.Algorithm',
+    'Sofa.Component.Collision.Detection.Intersection',
+    'Sofa.Component.Collision.Geometry',
+    'Sofa.Component.Collision.Response.Contact',
+    'Sofa.Component.Constraint.Projective',
+    'Sofa.Component.IO.Mesh',
+    'Sofa.Component.LinearSolver.Iterative',
+    'Sofa.Component.Mapping.Linear',
+    'Sofa.Component.Mass',
+    'Sofa.Component.ODESolver.Backward',
+    'Sofa.Component.SolidMechanics.FEM.Elastic',
+    'Sofa.Component.StateContainer',
+    'Sofa.Component.Topology.Container.Dynamic',
+    'Sofa.Component.Visual',
+    'Sofa.GL.Component.Rendering3D',
+    'Sofa.Component.AnimationLoop',
+    'Sofa.Component.LinearSolver.Direct',
+    'Sofa.Component.Constraint.Lagrangian.Correction',
+    'Sofa.Component.Topology.Mapping',
+    'Sofa.Component.MechanicalLoad'
+    ])
+
+    if SHOW_FORCE:
+        root.addObject('VisualStyle', displayFlags="showCollisionModels hideVisualModels showForceFields")
+
+    root.addObject('FreeMotionAnimationLoop')
+    root.addObject('GenericConstraintSolver', maxIterations=1000, tolerance=1e-6)
+
+    root.addObject('CollisionPipeline', name="CollisionPipeline")
+    root.addObject('BruteForceBroadPhase', name="BroadPhase")
+    root.addObject('BVHNarrowPhase', name="NarrowPhase")
+    root.addObject('DefaultContactManager', name="CollisionResponse", response="FrictionContactConstraint")
+    root.addObject('MinProximityIntersection', useLineLine=True, usePointPoint=True, alarmDistance=0.3, contactDistance=0.15, useLinePoint=True)
+
+    beam = createElasticObject(root, name=NAME, poissonRatio=POISSON_RATIO, youngModulus=YOUNG_MODULUS, magneticForce=MAGNETIC_FORCE, magneticDir=MAGNETIC_DIR, showForce=SHOW_FORCE)
+    return root
+
+
+
+# Function used only if this script is called from a python environment
+if __name__ == '__main__':
+    main()
