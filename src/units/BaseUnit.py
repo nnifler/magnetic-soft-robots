0e776d575054ef6e2b3fc254a1b7938de55d994b
Finn <108729205+LordFrostwolf@users.noreply.github.com>
2024-12-05 19:32:48 +0100

    Add comments

--- src/units/BaseUnit.py
+++ src/units/BaseUnit.py
@@ -1,7 +1,10 @@
 class BaseUnit:
     _value = 0
+
+    ## Basic init method to create a BaseUnit Object with the given value
     def __init__(self, value):
         self._value = value
 
+    ## String representation
     def __repr__(self) -> str:
         return f'{self._value}'
