8aa01f56e1a3e3e768f42c2e4d3080187059f5f9
Finn <108729205+LordFrostwolf@users.noreply.github.com>
2024-12-05 18:15:53 +0100

    add tests

---
+++ tests/units/YoungsModulusTest.py
@@ -0,0 +1,91 @@
+import unittest
+from src.units.YoungsModulus import YoungsModulus
+from random import randrange
+
+class TestInitMethods(unittest.TestCase):
+
+    def testPa(self):
+        val = randrange(1000)
+        uut = YoungsModulus.Pa(val)
+        self.assertEqual(val, uut.Pa)
+
+    def testhPa(self):
+        val = randrange(1000)
+        uut = YoungsModulus.hPa(val)
+        self.assertEqual(val, uut.hPa)
+
+    def testMPa(self):
+        val = randrange(1000)
+        uut = YoungsModulus.MPa(val)
+        self.assertEqual(val, uut.MPa)
+
+    def testGPa(self):
+        val = randrange(1000)
+        uut = YoungsModulus.GPa(val)
+        self.assertEqual(val, uut.GPa)
+
+class TestSetMethods(unittest.TestCase):
+
+    def testPa(self):
+        val = randrange(1, 1000)
+        uut = YoungsModulus(0)
+        uut.Pa = val
+        self.assertEqual(val, uut.Pa)
+
+
+    def testhPa(self):
+        val = randrange(1, 1000)
+        uut = YoungsModulus(0)
+        uut.hPa = val
+        self.assertEqual(val, uut.hPa)
+
+    def testMPa(self):
+        val = randrange(1, 1000)
+        uut = YoungsModulus(0)
+        uut.MPa = val
+        self.assertEqual(val, uut.MPa)
+
+    def testGPa(self):
+        val = randrange(1, 1000)
+        uut = YoungsModulus(0)
+        uut.GPa = val
+        self.assertEqual(val, uut.GPa)
+
+class TestExceptionalBehavior(unittest.TestCase):
+    
+    def testNegativeInit(self):
+        val = randrange(-1000,-1)
+        with self.assertRaises(ValueError):
+            YoungsModulus(val)
+        with self.assertRaises(ValueError):
+            YoungsModulus.Pa(val)
+        with self.assertRaises(ValueError):
+            YoungsModulus.hPa(val)
+        with self.assertRaises(ValueError):
+            YoungsModulus.MPa(val)
+        with self.assertRaises(ValueError):
+            YoungsModulus.GPa(val)
+
+    def testNegativeSet(self):
+        val = randrange(-1000,-1)
+        uut = YoungsModulus(0)
+        with self.assertRaises(ValueError):
+            uut.Pa(val)
+        with self.assertRaises(ValueError):
+            uut.hPa(val)
+        with self.assertRaises(ValueError):
+            uut.MPa(val)
+        with self.assertRaises(ValueError):
+            uut.GPa(val)
+
+class TestConversion(unittest.TestCase):
+    
+    def testPa_hPa(self):
+        val = randrange(1000)
+        uut = YoungsModulus.Pa(val)
+        self.assertEqual(val * 100, uut.hPa)
+
+    def testMpa_GPa(self):
+        val = randrange(1000)
+        uut = YoungsModulus.MPa(val)
+        self.assertEqual(val * 1000, uut.GPa)
