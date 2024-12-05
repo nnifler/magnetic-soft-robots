25d13ea0cd98aa4e31605c4ad32cfe43ded5ecbb
Julius <juliushahnewald@gmail.com>
2024-12-05 17:29:13 +0100

    Implement Heiko's requests

--- src/SceneBuilder.py
+++ src/SceneBuilder.py
@@ -1,55 +1,41 @@
 import Sofa
-from config import * 
+import config
+from typing import List, SupportsFloat
 
 
 class SceneBuilder():
     def __init__(self, 
             root,
-            gravity_vec = [0, -9.81, 0],
+            gravity_vec = config.GRAVITY_VEC,
             dt = 0.005,
             ) -> None:
         self.root = root
-        self.root.gravity = gravity_vec if USE_GRAVITY else [0]*3
         self.root.dt = dt
 
-        self.__build()
+        for x in gravity_vec: 
+            if not isinstance(x, SupportsFloat): raise TypeError(f"{x} has illegal argument type {type(x)} for gravity component")
+        if not len(gravity_vec) == 3: raise ValueError("invalid length for gravity vector")
+
+        self.root.gravity = [0]*3
+        if config.USE_GRAVITY:
+            self.root.gravity = gravity_vec 
+        self._build()
 
 
-    def __build(self):
-        self.__load_plugins()
-        if SHOW_FORCE: 
-            self.__render_force()
-        self.__setup_root_simulation()
+    def _build(self):
+        self._load_plugins()
+        if config.SHOW_FORCE: 
+            self._render_force()
+        self._setup_root_simulation()
 
         return self.root
 
 
-    def __load_plugins(self):
-        self.root.addObject("RequiredPlugin", pluginName=[
-            'Sofa.Component.Collision.Detection.Algorithm',
-            'Sofa.Component.Collision.Detection.Intersection',
-            'Sofa.Component.Collision.Geometry',
-            'Sofa.Component.Collision.Response.Contact',
-            'Sofa.Component.Constraint.Projective',
-            'Sofa.Component.IO.Mesh',
-            'Sofa.Component.LinearSolver.Iterative',
-            'Sofa.Component.Mapping.Linear',
-            'Sofa.Component.Mass',
-            'Sofa.Component.ODESolver.Backward',
-            'Sofa.Component.SolidMechanics.FEM.Elastic',
-            'Sofa.Component.StateContainer',
-            'Sofa.Component.Topology.Container.Dynamic',
-            'Sofa.Component.Visual',
-            'Sofa.GL.Component.Rendering3D',
-            'Sofa.Component.AnimationLoop',
-            'Sofa.Component.LinearSolver.Direct',
-            'Sofa.Component.Constraint.Lagrangian.Correction',
-            'Sofa.Component.Topology.Mapping',
-            'Sofa.Component.MechanicalLoad'
-            ])
+    def _load_plugins(self):
+        self.root.addObject("RequiredPlugin", pluginName=config.PLUGIN_LIST)
 
 
-    def __setup_root_simulation(self):
+    def _setup_root_simulation(self):
         self.root.addObject('FreeMotionAnimationLoop')
         self.root.addObject('GenericConstraintSolver', maxIterations=1000, tolerance=1e-6)
 
@@ -60,5 +46,5 @@ class SceneBuilder():
         self.root.addObject('MinProximityIntersection', useLineLine=True, usePointPoint=True, alarmDistance=0.3, contactDistance=0.15, useLinePoint=True)
 
 
-    def __render_force(self):
+    def _render_force(self):
         self.root.addObject('VisualStyle', displayFlags="showCollisionModels hideVisualModels showForceFields")
--- src/config.py
+++ src/config.py
@@ -1,9 +1,36 @@
+from numpy import float32
+
 # Choose Parameters
 USE_GUI = True
 USE_GRAVITY = True
+GRAVITY_VEC = [0, float(-10), 'a']
 NAME = "beam"
 POISSON_RATIO = 0.45
 YOUNG_MODULUS = 10000
 MAGNETIC_FORCE = 0
 MAGNETIC_DIR = (0,-1,0)
 SHOW_FORCE = False
+
+
+### Plugins ### 
+PLUGIN_LIST = ['Sofa.Component.Collision.Detection.Algorithm',
+    'Sofa.Component.Collision.Detection.Intersection',
+    'Sofa.Component.Collision.Geometry',
+    'Sofa.Component.Collision.Response.Contact',
+    'Sofa.Component.Constraint.Projective',
+    'Sofa.Component.IO.Mesh',
+    'Sofa.Component.LinearSolver.Iterative',
+    'Sofa.Component.Mapping.Linear',
+    'Sofa.Component.Mass',
+    'Sofa.Component.ODESolver.Backward',
+    'Sofa.Component.SolidMechanics.FEM.Elastic',
+    'Sofa.Component.StateContainer',
+    'Sofa.Component.Topology.Container.Dynamic',
+    'Sofa.Component.Visual',
+    'Sofa.GL.Component.Rendering3D',
+    'Sofa.Component.AnimationLoop',
+    'Sofa.Component.LinearSolver.Direct',
+    'Sofa.Component.Constraint.Lagrangian.Correction',
+    'Sofa.Component.Topology.Mapping',
+    'Sofa.Component.MechanicalLoad'
+]
