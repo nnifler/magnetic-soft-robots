c7fd085aaffa11b6e205d3302e7f1baeccd9fe73
Finn <108729205+LordFrostwolf@users.noreply.github.com>
2024-12-09 13:30:14 +0100

    add 3 more example materials

--- src/config.py
+++ src/config.py
@@ -7,11 +7,32 @@ USE_GUI = True
 USE_GRAVITY = True
 GRAVITY_VEC = [0, float(-10), 0]
 NAME = "beam"
+
+## Silicone rubber
 POISSON_RATIO = 0.47
 YOUNGS_MODULUS = YoungsModulus.fromGPa(0.001)
 DENSITY = Density.fromMgpm3(1.1)
+MAGNETIC_FORCE = 0.5
+
+## Rubber
+#POISSON_RATIO = 0.47
+#YOUNGS_MODULUS = YoungsModulus.fromGPa(0.05)
+#DENSITY = Density.fromgpcm3(1.5)
+#MAGNETIC_FORCE = 0.5
+
+## Low Density Polyethylene
+#POISSON_RATIO = 0.4
+#YOUNGS_MODULUS = YoungsModulus.fromGPa(0.228)
+#DENSITY = Density.fromgpcm3(0.925)
+#MAGNETIC_FORCE = 100
+
+## PET
+#POISSON_RATIO = 0.337
+#YOUNGS_MODULUS = YoungsModulus.fromGPa(3.14)
+#DENSITY = Density.fromgpcm3(1.63)
+#MAGNETIC_FORCE = 100
+
 SCALE = 0.02
-MAGNETIC_FORCE = 0.1
 MAGNETIC_DIR = (0,-1,0)
 SHOW_FORCE = False
 
