521b994f00758aad52c6d6aaee5d99a02955521d
Finn <108729205+LordFrostwolf@users.noreply.github.com>
2025-01-07 15:32:51 +0100

    make helper methods static and fix random angle test

--- tests/MagneticControllerTest.py
+++ tests/MagneticControllerTest.py
@@ -9,24 +9,24 @@ from scipy.spatial.transform import Rotation
 class TestAngles(unittest.TestCase):
 
     def testRightAngle(self):
-        actual = MagneticController.calculate_angle(None, [0,0,-1], [0,1,0], lambda x: x[1:])
+        actual = MagneticController.calculate_angle([0,0,-1], [0,1,0], 'x')
         ref = np.pi / 2
         self.assertAlmostEqual(actual, ref)
 
     def testParallel(self):
-        actual = MagneticController.calculate_angle(None, [0,0,1], [0,0,1], lambda x: x[1:])
+        actual = MagneticController.calculate_angle([0,0,1], [0,0,1], 'x')
         ref = 0
         self.assertAlmostEqual(actual, ref)
 
     def testAntiParallel(self):
-        actual = MagneticController.calculate_angle(None, [0,0,-1], [0,0,1], lambda x: x[1:])
+        actual = MagneticController.calculate_angle([0,0,-1], [0,0,1], 'x')
         ref = np.pi
         self.assertAlmostEqual(np.abs(actual), ref)
 
     def testSignChange(self):
         a,b = [0, 0, -1], [0, 1, 0]
-        actual1 = MagneticController.calculate_angle(None, a,b, lambda x: x[1:])
-        actual2 = MagneticController.calculate_angle(None, b,a, lambda x: x[1:])
+        actual1 = MagneticController.calculate_angle(a,b, 'x')
+        actual2 = MagneticController.calculate_angle(b,a, 'x')
         self.assertAlmostEqual(actual1, -1*actual2)
 
     def testRandomized(self):
@@ -37,37 +37,37 @@ class TestAngles(unittest.TestCase):
             r = Rotation.from_euler('xyz', rand_angs, degrees=False)
             rotated_vec = r.apply(rand_vec)
 
-            for i, subscript in enumerate([lambda x: x[1:], lambda x: x[::2], lambda x: x[:2]]):
-                a = MagneticController.calculate_angle(None, rand_vec, rotated_vec, subscript)
+            for i, subscript in enumerate(['x', lambda x: x[::2], lambda x: x[:2]]):
+                a = MagneticController.calculate_angle(rand_vec, rotated_vec, subscript)
                 self.assertAlmostEqual(a, rand_angs[i], msg=f"index {i}")
         # was hier passiert stimmt nicht. die winkel werden im nachhinein nochmal verändert werden.
         # stell dir vor, du schaust aus einer Richtung auf einen 45 grad winkel und drehst dann nochmal um die vertikale achse. 
         # dann spitzt sich der winkel der projektion logischerweise zu, da er ja irgendwann 0 werden muss (wenn der vektor so steht dass wir gerade draufsehen)
         # dh der erste winkel ist nicht notwendigerweise nach der letzten rotation so, wie wir ihn initial durchs drehen gebaut haben
 
-        for i, subscript in enumerate([lambda x: x[1:], lambda x: x[::2], lambda x: x[:2]]):
+        for i, subscript in enumerate('xyz'):
             one_axis = 'xyz'[i]
             one_angle = rand_angs[i]
 
             r = Rotation.from_euler(one_axis, one_angle, degrees=False)
             rotated_vec = r.apply(rand_vec)
 
-            a = MagneticController.calculate_angle(None, rand_vec, rotated_vec, subscript) # sign error still persists, swapping order of compared vecs only changes axis in which they occur (y, if swapped x)
+            a = MagneticController.calculate_angle(rand_vec, rotated_vec, subscript) # sign error still persists, swapping order of compared vecs only changes axis in which they occur (y, if swapped x)
             self.assertAlmostEqual(a, one_angle, msg=f"axis {one_axis}")
 
     def testOne0(self):
         rand_vec = (np.random.rand(3) - 0.5) * 20
-        actual = MagneticController.calculate_angle(None, [0,0,0], rand_vec, lambda x: x[1:])
+        actual = MagneticController.calculate_angle([0,0,0], rand_vec, 'x')
         ref = 0
         self.assertAlmostEqual(np.abs(actual), ref)
 
         rand_vec = (np.random.rand(3) - 0.5) * 20
-        actual = MagneticController.calculate_angle(None, rand_vec, [0,0,0], lambda x: x[1:])
+        actual = MagneticController.calculate_angle(rand_vec, [0,0,0], 'x')
         ref = 0
         self.assertAlmostEqual(np.abs(actual), ref)
 
     def testBoth0(self):
-        actual = MagneticController.calculate_angle(None, [0,0,0], [0,0,0], lambda x: x[1:])
+        actual = MagneticController.calculate_angle([0,0,0], [0,0,0], 'x')
         ref = 0
         self.assertAlmostEqual(np.abs(actual), ref)
 
@@ -80,7 +80,7 @@ class TestRotation(unittest.TestCase):
         reference_rotation = Rotation.from_euler("xyz", rand_angles, degrees=False)
         reference_vector = reference_rotation.apply(rand_base_vector)
 
-        rot_under_test = MagneticController.calculate_rotation(None, rand_base_vector, reference_vector)
+        rot_under_test = MagneticController.calculate_rotation(rand_base_vector, reference_vector)
         result_under_test = rot_under_test.apply(rand_base_vector)
 
         for pair in zip(reference_vector, result_under_test):
@@ -93,7 +93,7 @@ class TestRotation(unittest.TestCase):
         rand_dipole = (np.random.rand(3) - 0.5) * 20
 
         make_comparable = lambda a : a.as_euler('xyz', degrees=False).tolist()
-        rot_under_test = MagneticController.calculate_rotation(None, rand_base_vector, rand_dipole)
+        rot_under_test = MagneticController.calculate_rotation(rand_base_vector, rand_dipole)
         rot_reference = Rotation.align_vectors(rand_base_vector, rand_dipole)[0]
 
         for pair in zip(make_comparable(rot_under_test), make_comparable(rot_reference)):
