f2c6970ca5f4702fded764f9c84fd922d0ae4928
Finn <108729205+LordFrostwolf@users.noreply.github.com>
2025-01-18 00:53:48 +0100

    Add normalization of direction vector in config class

--- gui/mainWindow.py
+++ gui/mainWindow.py
@@ -307,15 +307,13 @@ class MainWindow(QMainWindow):
         remanence = self.remanence_spinbox.value()
         field_strength = self.field_strength_slider.value() / 10  # Umrechnung in Tesla
 
-        normalized_dir = np.array(direction) / np.linalg.norm(np.array(direction))
-
         Config.set_show_force(True)
         Config.set_model("butterfly_disected",
                          0.02)
         Config.set_external_forces(True,
                                    np.array([0, -9.81, 0]),
                                    field_strength,
-                                   normalized_dir,
+                                   np.array(direction),
                                    np.array([1, 0, 0]))
         Config.set_material_parameters(poisson_ratio,
                                        youngs_modulus,
