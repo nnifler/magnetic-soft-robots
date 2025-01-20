ca972c29c52901a7b7ca6afb3d0160de6ea482e1
Finn <108729205+LordFrostwolf@users.noreply.github.com>
2025-01-20 21:09:37 +0100

    rename magnetic_controller_test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 from .config_test import suite as config_test_suite
-from .MagneticControllerTest import suite as magnetic_controller_test_suite
+from .magnetic_controller_test import suite as magnetic_controller_test_suite
 from .material_loader_test import suite as material_loader_test_suite
 from .mesh_loader_test import suite as mesh_loader_test_suite
 from .scene_builder_test import suite as scene_builder_test_suite
