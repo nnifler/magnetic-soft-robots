4c35aea5356cb1b2a0b6995b5f5cebc80e43d159
Heiko Hellkamp <heiko@hellkamp.de>
2025-01-20 19:05:09 +0100

    refactor units pep

--- src/units/density.py
+++ src/units/density.py
@@ -1,56 +1,109 @@
+"""This module contains the Density class which is the unit class for Density."""
+
 from __future__ import annotations
-from src.units.BaseUnit import BaseUnit
 from enum import Enum
+from . import BaseUnit
 
 
-class Density(BaseUnit): # base: kgpm3
-    
-    ## Define conversion factors
+class Density(BaseUnit):
+    """Density unit class. Base unit is kg/m^3."""
+
     class Unit(Enum):
+        """Enum class for Density unit conversion factors."""
         kgpm3 = 1
         gpcm3 = 1_000
         Mgpm3 = 1_000
         tpm3 = 1_000
 
-    ## String representation
     def __repr__(self) -> str:
-        return f'{super().__repr__()} kg/m^3'
+        """Returns the string representation of the Density object.
 
-    ## Create a Density Object from a value with kg/m^3 as its unit
+        Returns:
+            str: The string representation of the Density object.
+        """
+        return f'{super().__repr__()} kg/m\u00b3'
+
     @staticmethod
-    def fromkgpm3(value: float) -> Density:
-        if value < 0: raise ValueError('Value cannot be negative!')
+    def from_kgpm3(value: float) -> Density:
+        """Creates a Density Object from a value with kg/m^3 as its unit.
+
+        Args:
+            value (float): The value of the Density Object in kg/m^3.
+
+        Raises:
+            ValueError: If the value is negative.
+
+        Returns:
+            Density: The Density Object with the given value.
+        """
+        if value < 0:
+            raise ValueError('Value cannot be negative!')
         return Density(value * Density.Unit.kgpm3.value)
 
-    ## Get the value of the Density Object in kg/m^3
     @property
     def kgpm3(self) -> float:
+        """Gets the value of the Density Object in kg/m^3.
+
+        Returns:
+            float: The value of the Density Object in kg/m^3.
+        """
         return self._value / self.Unit.kgpm3.value
-    
-    ## Set the value of the Density Object with a value with kg/m^3 as its unit
+
     @kgpm3.setter
     def kgpm3(self, value: int) -> None:
-        if value < 0: raise ValueError('Value cannot be negative!')
+        """Sets the value of the Density Object with a value with kg/m^3 as its unit.
+
+        Args:
+            value (int): The value of the Density Object in kg/m^3.
+
+        Raises:
+            ValueError: If the value is negative.
+        """
+        if value < 0:
+            raise ValueError('Value cannot be negative!')
         self._value = value * self.Unit.kgpm3.value
-    
-    ## Create a Density Object from a value with g/cm^3 as its unit
+
     @staticmethod
-    def fromgpcm3(value: float) -> Density:
-        if value < 0: raise ValueError('Value cannot be negative!')
+    def from_gpcm3(value: float) -> Density:
+        """Creates a Density Object from a value with g/cm^3 as its unit.
+
+        Args:
+            value (float): The value of the Density Object in g/cm^3.
+
+        Raises:
+            ValueError: If the value is negative.
+
+        Returns:
+            Density: The Density Object with the given value.
+        """
+        if value < 0:
+            raise ValueError('Value cannot be negative!')
         return Density(value * Density.Unit.gpcm3.value)
 
-    ## Get the value of the Density Object in g/cm^3
     @property
     def gpcm3(self) -> float:
+        """Gets the value of the Density Object in g/cm^3.
+
+        Returns:
+            float: _description_
+        """
         return self._value / self.Unit.gpcm3.value
-    
-    ## Set the value of the Density Object with a value with g/cm^3 as its unit
+
     @gpcm3.setter
     def gpcm3(self, value: float) -> None:
-        if value < 0: raise ValueError('Value cannot be negative!')
+        """Sets the value of the Density Object with a value with g/cm^3 as its unit.
+
+        Args:
+            value (float): The value of the Density Object in g/cm^3.
+
+        Raises:
+            ValueError: If the value is negative.
+        """
+        if value < 0:
+            raise ValueError('Value cannot be negative!')
         self._value = value * self.Unit.gpcm3.value
 
-    ## Define getter and setter for t/m^3 and Mg/m^3
+    # Define getter and setter for t/m^3 and Mg/m^3
     tpm3 = Mgpm3 = gpcm3
-    ## Define create methods for t/m^3 and Mg/m^3
-    fromtpm3 = fromMgpm3 = fromgpcm3
+    # Define create methods for t/m^3 and Mg/m^3
+    from_tpm3 = from_Mgpm3 = from_gpcm3
--- src/units/tesla.py
+++ src/units/tesla.py
@@ -1,31 +1,60 @@
+"""This module contains the Tesla class which is the unit class for Tesla."""
+
 from __future__ import annotations
-from src.units.BaseUnit import BaseUnit
 from enum import Enum
+from . import BaseUnit
 
-class Tesla(BaseUnit): # base: T
 
-    def __init__(self, value):
+class Tesla(BaseUnit):
+    """Unit class for Tesla. The base unit is T."""
+
+    def __init__(self, value: float) -> None:
+        """Initializes the Tesla object with the given value.
+
+        Args:
+            value (float): The init base value of the unit.
+        """
+        super().__init__(0)  # Initialize super object with non-negative value
         self._value = value
 
-    ## Define conversion factors
     class Unit(Enum):
+        """Enum class for Tesla unit conversion factors."""
         T = 1
 
-    ## String representation
     def __repr__(self) -> str:
+        """Returns the string representation of the Tesla object.
+
+        Returns:
+            str: The string representation of the Tesla object.
+        """
         return f'{super().__repr__()} T'
-    
-    ## Create a Tesla Object from a value with T as its unit
+
     @staticmethod
-    def fromT(value: float) -> Tesla:
+    def from_T(value: float) -> Tesla:
+        """Creates a Tesla Object from a value with T as its unit.
+
+        Args:
+            value (float): The value of the Tesla Object in T.
+
+        Returns:
+            Tesla: The Tesla Object with the given value.
+        """
         return Tesla(value * Tesla.Unit.T.value)
 
-    ## Get the value of the Tesla Object in T
     @property
     def T(self) -> float:
+        """Gets the value of the Tesla Object in T.
+
+        Returns:
+            float: The value of the Tesla Object in T.
+        """
         return self._value / self.Unit.T.value
-    
-    ## Set the value of the Tesla Object with a value with T as its unit
+
     @T.setter
     def T(self, value: int) -> None:
+        """Sets the value of the Tesla Object with a value with T as its unit.
+
+        Args:
+            value (int): The value of the Tesla Object in T.
+        """
         self._value = value * self.Unit.T.value
