31ee67dcfdf5f9bdc1990aca68f0a48fb29d36a5
Heiko Hellkamp <heiko@hellkamp.de>
2025-01-20 20:13:47 +0100

    refactor loaders, builders, objects

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 """This module is the main module of the package. It imports all the other modules and classes."""
+
 from .config import Config
 from .elastic_object import ElasticObject
 from .magnetic_controller import MagneticController
--- src/elastic_object.py
+++ src/elastic_object.py
@@ -1,14 +1,27 @@
-from src.units.YoungsModulus import YoungsModulus
-from src.units.Density import Density
-from src.mesh_loader import MeshLoader, Mode
-from src.config import Config
+"""This module contains the ElasticObject class,
+   which is used to create the elastic object that is simulated in the MSR."""
+
+import Sofa.Core
+from . import MeshLoader, Config
+from .mesh_loader import Mode
+from .units import YoungsModulus, Density
+
 
 class ElasticObject():
-    '''
-    Class that holds important parameters for the elastic object - the MSR - that is simulated.
-    Upon class initialization, code from 
-    '''
-    def __init__(self, root, mesh_loader: MeshLoader, poissonRatio: float, youngsModulus: YoungsModulus, density: Density):
+    """Class that holds important parameters for
+       the elastic object - the MSR - that is simulated."""
+
+    def __init__(self, root: Sofa.Core.Node, mesh_loader: MeshLoader, poisson_ratio: float,
+                 youngs_modulus: YoungsModulus, density: Density):
+        """Initializes the ElasticObject with the given parameters.
+
+        Args:
+            root (Sofa.Core.Node): The root node of the scene.
+            mesh_loader (MeshLoader): The mesh loader object.
+            poisson_ratio (float): The Poisson's ratio of the object.
+            youngs_modulus (YoungsModulus): The Young's modulus of the object.
+            density (Density): The density of the object.
+        """
         self._mesh_loader = mesh_loader
         self._root = root
 
@@ -21,48 +34,72 @@ class ElasticObject():
         self.diagonal_mass = None
         self.remanence = Config.get_remanence()
 
-
-        ## Add Object
+        # Add Object
         eo_node = self._root.addChild('object')
         self.node = eo_node
-        eo_node.addObject('EulerImplicitSolver', name="cg_odesolver", rayleighStiffness=0.1, rayleighMass=0.1)
-        eo_node.addObject('SparseLDLSolver', name="linear_solver", template="CompressedRowSparseMatrixMat3x3d")
+        eo_node.addObject('EulerImplicitSolver', name="cg_odesolver",
+                          rayleighStiffness=0.1, rayleighMass=0.1)
+        eo_node.addObject('SparseLDLSolver', name="linear_solver",
+                          template="CompressedRowSparseMatrixMat3x3d")
 
         self.mesh = self._mesh_loader.load_mesh_into(eo_node, Mode.VOLUMETRIC)
-        eo_node.addObject('TetrahedronSetTopologyContainer', name="topo", src=self._mesh_loader.reference(Mode.VOLUMETRIC))
-        self.mech_obj = eo_node.addObject('MechanicalObject', name="dofs", src=self._mesh_loader.reference(Mode.VOLUMETRIC))
-        eo_node.addObject('TetrahedronSetGeometryAlgorithms', template="Vec3d", name="GeomAlgo")
-        self.diagonal_mass = eo_node.addObject('DiagonalMass', name="Mass", massDensity=density.kgpm3)
-        self.FEM_force_field = eo_node.addObject('TetrahedralCorotationalFEMForceField', template="Vec3d", name="FEM", method="large", poissonRatio=poissonRatio, youngModulus=youngsModulus.Pa, computeGlobalMatrix=False)
+        eo_node.addObject('TetrahedronSetTopologyContainer', name="topo",
+                          src=self._mesh_loader.reference(Mode.VOLUMETRIC))
+        self.mech_obj = eo_node.addObject(
+            'MechanicalObject', name="dofs", src=self._mesh_loader.reference(Mode.VOLUMETRIC))
+        eo_node.addObject('TetrahedronSetGeometryAlgorithms',
+                          template="Vec3d", name="GeomAlgo")
+        self.diagonal_mass = eo_node.addObject(
+            'DiagonalMass', name="Mass", massDensity=density.kgpm3)
+        self.FEM_force_field = eo_node.addObject(
+            'TetrahedralCorotationalFEMForceField',
+            template="Vec3d",
+            name="FEM",
+            method="large",
+            poissonRatio=poisson_ratio,
+            youngModulus=youngs_modulus.Pa,
+            computeGlobalMatrix=False
+        )
 
-        ## Add Constraints
+        # Add Constraints
         positions = self.mesh.position.value.tolist()
-        ind = [i for i in range(len(positions)) if positions[i][0] == 0]
+        ind = [idx for idx, pos in positions if pos[0] == 0]
         constraints = " ".join(str(x) for x in ind)
-        eo_node.addObject('FixedConstraint', name="FixedConstraint", indices=constraints)
+        eo_node.addObject('FixedConstraint',
+                          name="FixedConstraint", indices=constraints)
         eo_node.addObject('LinearSolverConstraintCorrection')
 
-        ## Add Surface
+        # Add Surface
         surf = eo_node.addChild('ExtractSurface')
-        surf.addObject('TriangleSetTopologyContainer', name="Container", position="@../topo.position")
+        surf.addObject('TriangleSetTopologyContainer',
+                       name="Container", position="@../topo.position")
         surf.addObject('TriangleSetTopologyModifier', name="Modifier")
-        surf.addObject('Tetra2TriangleTopologicalMapping', name="SurfaceExtractMapping", input="@../topo", output="@Container")
+        surf.addObject('Tetra2TriangleTopologicalMapping',
+                       name="SurfaceExtractMapping", input="@../topo", output="@Container")
 
-        ## Add collision
+        # Add collision
         collision = surf.addChild('Surf')
-        collision.addObject('TriangleSetTopologyContainer', name="Container", src="@../Container")
+        collision.addObject('TriangleSetTopologyContainer',
+                            name="Container", src="@../Container")
         collision.addObject('MechanicalObject', name="surfaceDOFs")
         collision.addObject('PointCollisionModel', name="CollisionModel")
-        collision.addObject('IdentityMapping', name="CollisionMapping", input="@../../dofs", output="@surfaceDOFs")
+        collision.addObject('IdentityMapping', name="CollisionMapping",
+                            input="@../../dofs", output="@surfaceDOFs")
 
-        ## Add visuals
+        # Add visuals
         visu = eo_node.addChild("VisualModel")
         visu.loader = self._mesh_loader.load_mesh_into(visu, Mode.SURFACE)
-        visu.addObject('OglModel', name="model", src=self._mesh_loader.reference(Mode.SURFACE), color=[1., 1., 1.], updateNormals=False)
+        visu.addObject('OglModel', name="model", src=self._mesh_loader.reference(
+            Mode.SURFACE), color=[1., 1., 1.], updateNormals=False)
         visu.addObject('BarycentricMapping')
 
         l = len(self.mesh.position.value)
         self.vertex_forces = [None] * l
         for i in range(l):
-            self.vertex_forces[i] = eo_node.addObject('ConstantForceField', indices = f"{i}", name=f"force_{i}", forces=[0,0,0], showArrowSize="0.001" if Config.get_show_force() else "0")
-
+            self.vertex_forces[i] = eo_node.addObject(
+                'ConstantForceField',
+                indices=f"{i}",
+                name=f"force_{i}",
+                forces=[0, 0, 0],
+                showArrowSize="0.001" if Config.get_show_force() else "0"
+            )
