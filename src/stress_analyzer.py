76104fcafac146f03c89cbe86e335253ac900041
Julius <juliushahnewald@gmail.com>
2025-02-24 14:12:54 +0100

    docstrings

--- src/stress_analyzer.py
+++ src/stress_analyzer.py
@@ -1,3 +1,6 @@
+"""Implementation of the Stress Analyzer, a class responsible for the von Mises stress analysis. It updates the associated GUI component as well."""
+from typing import Any
+
 import Sofa
 import numpy as np
 
@@ -5,11 +8,15 @@ from src import AnalysisParameters, ElasticObject
 
 
 class StressAnalyzer(Sofa.Core.Controller):
+    """Analyzer responsible for the von Mises stress analysis. 
+    Updates max_stress and min_stress on each step.
+    """
+
     def __init__(self, elastic_object: ElasticObject, parameters: AnalysisParameters) -> None:
         super().__init__()
 
         if elastic_object is None or parameters is None:
-            raise ValueError("provided Parameter is None")
+            raise ValueError("provided argument is None")
 
         self._elastic_object = elastic_object
         self._params = parameters
@@ -18,7 +25,12 @@ class StressAnalyzer(Sofa.Core.Controller):
         self.min_stress = np.inf
 
     # override -> no snake case
-    def onAnimateBeginEvent(self, _) -> None:
+    def onAnimateBeginEvent(self, _: Any) -> None:
+        """Overrides the onAnimateBeginEvent method executed before each animation step.
+
+        Args:
+            _ (Any): the (unused) event
+        """
         if not self._params.stress_analysis:
             return
 
