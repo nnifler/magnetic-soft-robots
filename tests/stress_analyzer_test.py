7041ca6fae85703b47e7b9daebf300b8c32fab41
Julius <juliushahnewald@gmail.com>
2025-02-24 11:10:18 +0100

    add error case test

--- tests/stress_analyzer_test.py
+++ tests/stress_analyzer_test.py
@@ -9,20 +9,17 @@ class TestStressAnalyzer(unittest.TestCase):
     def test_regular_behaviour(self):
         eo_mock = unittest.mock.MagicMock()
         von_mises_vals = [
-            # "dummy call",
             [0]*5,
             [1]*5,
             [-0.3]*5,
             [3.8]*7,
-            [5] * 5,
+            [5, 5, 5, 7.2, 5],
             [4.5]*5,
         ]
 
         type(eo_mock.FEM_force_field.vonMisesPerNode).value = unittest.mock.PropertyMock(
             side_effect=von_mises_vals
         )
-
-        # print(eo_mock.FEM_force_field.vonMisesPerNode.value)
 
         para_mock = unittest.mock.MagicMock()
         para_mock.stress_analysis = True
@@ -31,8 +28,17 @@ class TestStressAnalyzer(unittest.TestCase):
         for _ in range(len(von_mises_vals)):
             uut.onAnimateBeginEvent(None)
 
-        self.assertEqual(5, uut.max_stress)
+        self.assertAlmostEqual(7.2, uut.max_stress)
         self.assertAlmostEqual(-0.3, uut.min_stress)
+
+    def test_none_args(self):
+        mock = unittest.mock.Mock()
+        with self.assertRaises(ValueError):
+            StressAnalyzer(None, None)
+        with self.assertRaises(ValueError):
+            StressAnalyzer(mock, None)
+        with self.assertRaises(ValueError):
+            StressAnalyzer(None, mock)
 
 
 def suite() -> unittest.TestSuite:
