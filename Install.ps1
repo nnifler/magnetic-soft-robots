ac47538319e5143cbc79d628411759e1394534a1
Heiko Hellkamp <heiko@hellkamp.de>
2025-03-17 10:00:19 +0100

    faster install script

--- Install.ps1
+++ Install.ps1
@@ -11,6 +11,7 @@ $get_pip_url = "https://bootstrap.pypa.io/get-pip.py"
 $py_archive = "python-$py_version-$arch.zip"
 $py_path = "python-$py_version-$arch"
 
+$global:ProgressPreference = 'SilentlyContinue'
 $py_version_a, $py_version_b, $py_version_c = $py_version.split(".")
 function Save-EnvFile {
     param (
@@ -30,23 +31,34 @@ function Save-EnvFile {
     Write-Host "Environment variables saved to $FilePath"
 }
 
+if ((Test-Path -Path $py_archive) -or (Test-Path -Path $py_path) -or (Test-Path -Path $sofa_archive) -or (Test-Path -Path $sofa_path) -or (Test-Path -Path "get_pip.py") -or (Test-Path -Path ".env")) {
+    Write-Host "The software is either already installed or a directory / file would be overridden by this install script" -ForegroundColor DarkRed
+    Write-Host "Aborting installation!" -ForegroundColor DarkRed
+    return
+}
+
+Write-Output "Downloading Python $py_version"
 Invoke-WebRequest -uri $py_url -Method "GET" -Outfile $py_archive
 Expand-Archive -Path $py_archive -DestinationPath $py_path
 
+Write-Output "Downloading SOFA $sofa_version"
 Invoke-WebRequest -uri $sofa_url -Method "GET" -Outfile $sofa_archive
 Expand-Archive -Path $sofa_archive -DestinationPath .
 
 Invoke-WebRequest -uri $get_pip_url -Method "GET" -Outfile get_pip.py
-& "$py_path/python.exe" get_pip.py
+& "$py_path/python.exe" get_pip.py --no-warn-script-location
 
 $pthFile = "$py_path\python$py_version_a$py_version_b._pth"
 Add-Content -Path $pthFile -Value "$pwd\$py_path\Lib\site-packages"
 Add-Content -Path $pthFile -Value "$pwd\$sofa_path\plugins\STLIB\lib\python3\site-packages"
 Add-Content -Path $pthFile -Value "$pwd\$sofa_path\plugins\SofaPython3\lib\python3\site-packages"
 Add-Content -Path $pthFile -Value "$pwd"
-& "$py_path\python.exe" -m pip install -r requirements.txt
+& "$py_path\python.exe" -m pip install -r requirements.txt --no-warn-script-location
 
 $sofa_root = "$pwd\$sofa_path"
 $python_root = "$pwd\$py_path"
 
 Save-EnvFile -FilePath .env -VariableNames @("sofa_root", "python_root")
+
+Write-Host "Installation finished!" -ForegroundColor DarkGreen
+Write-Host "Execute the software by running Run.ps1" -ForegroundColor DarkGreen
--- Run.ps1
+++ Run.ps1
@@ -1,6 +1,18 @@
+if (!(Test-Path -Path ".env")) {
+    Write-Host "Necessary data for running the software is missing" -ForegroundColor DarkRed
+    Write-Host "Consider running or re-running the install script (Install.ps1)" -ForegroundColor DarkRed
+    return
+}
+
 get-content .env | ForEach-Object {
     $name, $value = $_.split('=')
     set-content env:\$name $value
+}
+
+if (!(Test-Path -Path $env:PYTHON_ROOT) -or !(Test-Path -Path $env:SOFA_ROOT)) {
+    Write-Host "Necessary data for running the software is missing" -ForegroundColor DarkRed
+    Write-Host "Consider running or re-running the install script (Install.ps1)" -ForegroundColor DarkRed
+    return
 }
 
 & "$env:PYTHON_ROOT\python.exe" .\main.py
